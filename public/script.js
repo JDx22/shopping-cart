f9f2d512d69a863bdde9f6f5de64c092f76fdc2b
erez berliner <erezberliner@h-MacBook-Pro-sl-erez.local>
2018-04-29 16:42:48 +0300

    added remove button

--- public/script.js
+++ public/script.js
@@ -10,7 +10,7 @@ var updateCart = function () {
   var i=0, cartTotal=0;
   for (i=0;i<cart.length;i++)
   {
-    $(".cart-list").append('<p> '+cart[i]['name']+' - '+cart[i]['price']+'$ x'+cart[i].quantity+'</p>');
+    $(".cart-list").append('<p data-name="'+cart[i].name+'"> '+cart[i]['name']+' - '+cart[i]['price']+'$ x'+cart[i].quantity+'   <button type="button" class="btn btn-danger remove-button">Remove</button></p>');
     cartTotal+=cart[i]['price']*cart[i].quantity;
   }
   
@@ -18,6 +18,7 @@ var updateCart = function () {
   $total.empty();
   $total.append(cartTotal)
 }
+
 
 
 var addItem = function (item) {
@@ -49,7 +50,15 @@ $('.view-cart').on('click', function () {
     return newVal;
   })
 });
+$('.cart-list').on('click','.remove-button',function(){
+  var name=$(this).closest('p').data().name;
+  var i=0;
+  for(; i< cart.length && cart[i].name!==name;i++);
+  if(i!==cart.length) //not necessery, but just in case there is  a bug
+    cart.splice(i,1);
+  updateCart();
 
+})
 $('.add-to-cart').on('click', function () {
   // TODO: get the "item" object from the page
   var $item = $(this).closest(".item");
